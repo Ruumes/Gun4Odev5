29910838979f82c04ed03db77e72306800997b4d
Ruumes <79170772+Ruumes@users.noreply.github.com>
2021-02-20 23:10:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionaryy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Students and Number of Students");
+            Console.WriteLine("");
+
+            MyDictionaryy<int, string> studentsNumber = new MyDictionaryy<int, string>();
+            Console.WriteLine("List of Student");
+            studentsNumber.Add(165, "Ayşe Aydın");
+            studentsNumber.Add(245, "Fatma Güler");
+            studentsNumber.Add(328, "Ali Enginn");
+            studentsNumber.Add(825, "Mehmet Çetin");
+            studentsNumber.Add(1026, "Hasan Çelik");
+
+            studentsNumber.List();
+            Console.WriteLine(" ");
+            Console.WriteLine("Number of Students");
+            Console.WriteLine(studentsNumber.Count());
+
+        }
+
+
+    }
+
+
+}
+
